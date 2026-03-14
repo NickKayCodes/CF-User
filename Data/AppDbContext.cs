f47c6c2ef2034a7bbd025d80a7d615e1f8d06195
Nicky Kayyarath <nkayyarath@gmail.com>
2026-03-14 19:05:47 -0400

    Setup DB connection

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CF_User.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CF_User.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users => Set<AppUser>();
+    }
+}
+
