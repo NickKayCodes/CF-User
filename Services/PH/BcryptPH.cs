220e4cc60b0a0b766a3f59f5c11d349621c6f713
Nicky Kayyarath <nkayyarath@gmail.com>
2026-03-14 19:05:58 -0400

    Setup for pw hashing

---
+++ Services/PH/BcryptPH.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CF_User.Services.PH
+{
+    public class BcryptPH : IPH
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public PasswordVerificationResult VerifyHashedPassword(string passwordHash, string password)
+        {
+            bool verified = BCrypt.Net.BCrypt.Verify(password, passwordHash);
+
+            return verified
+                ? PasswordVerificationResult.Success
+                : PasswordVerificationResult.Failed;
+        }
+
+    }
+}
---
+++ Services/PH/IPH.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CF_User.Services.PH
+{
+    public interface IPH
+    {
+            string HashPassword(string password);
+            PasswordVerificationResult VerifyHashedPassword(string passwordHash, string password);
+    }
+}
